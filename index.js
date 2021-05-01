9440814e90b6f77f858ffe8cb61f13f16d0b189e
ubaig517 <ubaig517>
2021-05-01 03:19:35 -0400

    Completed timer functionality and styling.

--- index.js
+++ index.js
@@ -1,58 +1,21 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    if (callbacks) {
-      this.onStart = callbacks.onStart;
-      this.onTick = callbacks.onTick;
-      this.onComplete = callbacks.onComplete;
-    }
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    if (this.onStart) this.onStart();
-
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      if (this.onComplete) this.onComplete();
-      this.pause();
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-      if (this.onTick) this.onTick();
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
 
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log("Timer started");
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log("Ticking");
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter
+    );
   },
   onComplete() {
     console.log("Timer Completed");
