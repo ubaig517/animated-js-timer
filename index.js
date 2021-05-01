4f46a8af0c6cb6aee949b3609027815ed4a32cff
ubaig517 <ubaig517>
2021-05-01 01:58:31 -0400

    Implemented code for timer start, pause and tick. Added optional parameters for Timer constructor.

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
 
-  start() {
-    console.log("Time to start timer");
+  start = () => {
+    if (this.onStart) this.onStart();
+
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      if (this.onComplete) this.onComplete();
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) this.onTick();
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
   }
 }
 
@@ -16,4 +47,14 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Ticking");
+  },
+  onComplete() {
+    console.log("Timer Completed");
+  },
+});
